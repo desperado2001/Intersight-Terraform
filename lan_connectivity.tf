989ab3560ede3a39036c9ddfd6d4d30f7ee14637
desperado2001 <ChristopherAlviar@gmail.com>
2021-08-19 11:18:21 +0800

    Create lan_connectivity.tf

---
+++ lan_connectivity.tf
@@ -0,0 +1,19 @@
+resource "intersight_vnic_lan_connectivity_policy" "vnic_lan1" {
+  name                = "vnic_lan1"
+  description         = "demo vnic lan connectivity policy"
+  iqn_allocation_type = "None"
+  placement_mode      = "auto"
+  target_platform     = "FIAttached"
+  organization {
+    object_type = "organization.Organization"
+    moid        = var.organization
+  }
+  profiles {
+    moid = intersight_server_profile.server1.moid
+    object_type = "server.Profile"
+  } 
+  organization {
+    object_type = "organization.Organization"
+    moid        = data.intersight_organization_organization.terra_demo.results[0].moid
+  }
+}
