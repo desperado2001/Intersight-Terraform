f0409edda20d0d68e9274edb50d057e900a99388
desperado2001 <ChristopherAlviar@gmail.com>
2021-08-19 13:55:58 +0800

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -17,13 +17,3 @@ provider "intersight" {
     secretkey = var.secretkey
     endpoint = "https://intersight.com"
 }
-
-provider "vsphere" {
-  user           = var.vsphere_user
-  password       = var.vsphere_password
-  vsphere_server = var.vsphere_server
-
-  # if you have a self-signed cert
-  allow_unverified_ssl = true
-}
-
